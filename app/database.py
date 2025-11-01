44d0fee7683faed3723b55b899f572cf66a0b157
jianyang21 <dshakthi2003@gmail.com>
2025-11-01 18:16:12 +0530

    Updated database.py to use .env and added .gitignore

--- app/database.py
+++ app/database.py
@@ -1,17 +1,24 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
 # ---------------------------------------------------
 # ⚙️  Direct Database URL (edit your credentials below)
 # ---------------------------------------------------
 
+<<<<<<< HEAD
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
+=======
+load_dotenv()
+>>>>>>> 3e5c3fe (Updated database.py to use .env and added .gitignore)
 
+DATABASE_URL = os.getenv("DATABASE_URL")
 # Create SQLAlchemy engine
 engine = create_engine(DATABASE_URL)
 
